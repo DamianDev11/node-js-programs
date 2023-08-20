50ef62faea77420093623ddc5de780cfd7230b12
Damian <damianprakash07@gmail.com>
2023-08-21 03:08:22 +0530

    writing CSV to file

---
+++ writingCSV/index.js
@@ -0,0 +1,18 @@
+import * as stringify from "csv-stringify";
+import fs from "fs";
+
+const write = fs.createWriteStream("log.csv");
+const data = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [10, 11, 12],
+];
+const columns = ["nums1", "num2", "num3"];
+const stringifier = stringify.stringify({ header: true, columns: columns });
+
+for (let i = 0; i < data.length; i++) {
+  stringifier.write(data[i]);
+}
+
+stringifier.pipe(write);
