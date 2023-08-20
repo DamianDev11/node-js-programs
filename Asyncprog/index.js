021a3898a889f260dbea543cba928f967c636753
Damian <damianprakash07@gmail.com>
2023-08-20 19:59:09 +0530

    Async programs

---
+++ Asyncprog/index.js
@@ -0,0 +1,11 @@
+import { error } from "console";
+import fs from "fs";
+
+fs.readFile("async.txt", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
+console.log("Done");
